3e0ba1bc2007214d6f217bf88f952f0c46f6c618
jungmin <kjmhs501@gmail.com>
2025-11-21 13:40:54 +0900

    feat: MainNavbar active 여부 스타일 공통 함수로 처리

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 const HomeLayout = () => {
   return (
     <div>
-      <MainNavbar />
+      <MainNavbar isLoggedIn={false} />
       <Outlet />
       <Footbar />
     </div>
