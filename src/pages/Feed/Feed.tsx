cae10243af09fbdafe9816e6f2aafc4b875de343
jungmin <kjmhs501@gmail.com>
2025-12-23 18:36:55 +0900

    feat: FeedCard 컴포넌트 제작

--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -1,7 +1,21 @@
+import Pagination from "@/components/common/Pagination";
+import FeedCard from "@/components/feed/FeedCard";
+
 const Feed = () => {
   return (
-    <div className="h-full pt-16 pb-16 bg-red-500">
-      <h2 className="text-2xl text-black">Feed</h2>
+    <div className="mb-[60px]">
+      <div className="h-full px-[340px] my-[60px] flex flex-col items-start justify-center gap-[60px]">
+        <FeedCard image="https://picsum.photos/200/300" onClick={() => {}} />
+        <FeedCard onClick={() => {}} />
+      </div>
+      <Pagination
+        className="px-[340px] w-full flex justify-center items-center"
+        totalItems={10}
+        pageSize={10}
+        visiblePages={5}
+        currentPage={1}
+        onChange={() => {}}
+      />
     </div>
   );
 };
