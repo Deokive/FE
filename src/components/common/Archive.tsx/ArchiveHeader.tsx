a4f56abca8628fb593a47dd3eb2e0ce556f71fc4
jungmin <kjmhs501@gmail.com>
2025-12-27 19:23:49 +0900

    feat: 아카이브 헤더 justify 설정 변경

--- src/components/common/Archive.tsx/ArchiveHeader.tsx
+++ src/components/common/Archive.tsx/ArchiveHeader.tsx
@@ -17,20 +17,22 @@ const ArchiveHeader = ({
   return (
     <div className="w-full flex flex-col items-start gap-[20px]">
       <p className="typo-h1 text-color-highest">{title}</p>
-      <div className="w-full h-[51px] flex items-center gap-[20px]">
+      <div className="w-full h-[51px] flex items-center justify-between gap-[20px]">
         {/* 아카이브 소유자 부분 */}
-        <div className="flex w-[951px] gap-[10px] items-center">
+        <div className="flex gap-[10px] items-center">
           <img src={UserIcon} alt="user" className="w-[40px] h-[40px]" />
-          <p className="typo-body2 text-color-high">{ownerNickname}</p>
+          <p className="typo-body2 text-color-high">
+            {ownerNickname ?? "사용자명"}
+          </p>
         </div>
         {/* 배지 */}
-        <div className="flex gap-[20px] justify-end">
+        <div className="flex gap-[20px] justify-center">
           <div className="flex justify-center items-center px-[20px] py-[10px] rounded-[10px] bg-surface-container-40 text-color-high">
             {badge}
           </div>
           {/* 디데이 부분 */}
           <div
-            className="flex justify-center items-end px-[10px] py-[4px] rounded-[4px]
+            className="flex justify-center items-center px-[10px] py-[4px] rounded-[4px]
       bg-[#B2BCC2] typo-h1 text-color-lowest"
           >
             + {getDPlusDay(createdAt ?? "")} Day
