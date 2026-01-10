c3d477829620357f2c42ec172fb36192cc0a69bb
rhdmschdl <lovelyskygir@naver.com>
2026-01-10 23:30:12 +0900

    feat: className prop 추가

--- src/components/archive/Empty/EmptyList.tsx
+++ src/components/archive/Empty/EmptyList.tsx
@@ -6,6 +6,7 @@ interface EmptyListProps {
   description?: string;
   onClick?: () => void;
   startIcon?: React.ReactNode;
+  className?: string;
 }
 
 const EmptyList = ({
@@ -13,11 +14,12 @@ const EmptyList = ({
   onClick,
   description,
   startIcon,
+  className = "",
 }: EmptyListProps) => {
   return (
     <div
-      className="w-310 h-75 px-2.5 py-22.5 flex flex-col items-center justify-center gap-10 
-  rounded-lg border-2 border-solid border-border-low"
+      className={`w-310 h-75 px-2.5 py-22.5 flex flex-col items-center justify-center gap-10 
+  rounded-lg border-2 border-solid border-border-low ${className}`}
     >
       <BtnIcon
         startIcon={startIcon ?? <Plus className="w-6 h-6 text-color-high" />}
