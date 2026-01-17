41f1c13f6cbbb77737dc29d807b2ef29f73d0874
strfunctionk <comzun01@gmail.com>
2026-01-17 15:46:42 +0900

    fix: 필요한 enum 값으로만 수정

--- src/enums/mimeType.ts
+++ src/enums/mimeType.ts
@@ -1,4 +1,3 @@
-// TODO: 백엔드랑 통일 되어있는 지 확인 필요 - mime type이라 일단 여러개 추가
 export enum MimeType {
   // Video
   VIDEO_MP4 = "video/mp4",
@@ -14,21 +13,9 @@ export enum MimeType {
   IMAGE_WEBP = "image/webp",
   IMAGE_SVG = "image/svg+xml",
 
-  // Audio
+  // Audio 추후 확장성을 위한 enum
   AUDIO_MP3 = "audio/mpeg",
   AUDIO_WAV = "audio/wav",
   AUDIO_OGG = "audio/ogg",
   AUDIO_AAC = "audio/aac",
-
-  // Document
-  APPLICATION_PDF = "application/pdf",
-  APPLICATION_JSON = "application/json",
-  APPLICATION_XML = "application/xml",
-  APPLICATION_ZIP = "application/zip",
-
-  // Text
-  TEXT_PLAIN = "text/plain",
-  TEXT_HTML = "text/html",
-  TEXT_CSS = "text/css",
-  TEXT_CSV = "text/csv",
 }
