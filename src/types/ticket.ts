2c1632f096d9e13a2261c9466e3221565ffc65e0
strfunctionk <comzun01@gmail.com>
2026-01-18 02:37:37 +0900

    feat: 타입 추가
    - 티켓 타입 추가
    
    - 페이지네이션 타입 및 enum 추가

--- src/types/ticket.ts
+++ src/types/ticket.ts
@@ -1,3 +1,12 @@
+import type { FileCompleteResponse } from "./file";
+import type {
+  DefaultPaginationRequest,
+  DefaultPaginationResponse,
+} from "./pagination";
+import { PaginationSort } from "@/enums/paginationSort";
+import { PaginationDirection } from "@/enums/paginationDirection";
+
+// 개발 편의를 위한 타입
 export type Ticket = {
   id: string;
   imageUrl?: string | null;
@@ -10,4 +19,82 @@ export type Ticket = {
   rating?: number | null;
   review?: string | null;
   createdAt?: string | null;
+};
+
+type TicketId = {
+  ticketid: number;
+};
+
+type ArchiveId = {
+  archiveId: number;
+};
+
+type DefaultTicketResponse = {
+  id: number;
+  title: string;
+  date: string;
+  location: string | null;
+  seat: string | null;
+  casting: string | null;
+  score: number | null;
+  review: string | null;
+  ticketBookId: number;
+  file: FileCompleteResponse | null;
+};
+
+type DefaultTicketRequest = {
+  title: string;
+  date: string;
+  location?: string | null;
+  seat?: string | null;
+  casting?: string | null;
+  score?: number | null;
+  review?: string | null;
+  fileId?: number | null;
+};
+
+type TicketBookContent = {
+  id: number;
+  title: string;
+  thumbnail: string | null;
+  date: string;
+  seat?: string | null;
+  location?: string | null;
+  casting?: string | null;
+  createdAt: string;
+  lastModifiedAt: string;
+};
+
+export type AddTicketRequest = ArchiveId & DefaultTicketRequest;
+
+export type AddTicketResponse = DefaultTicketResponse;
+export type GetTicketRequest = TicketId;
+export type GetTicketResponse = DefaultTicketResponse;
+
+export type DeleteTicketRequest = TicketId;
+export type DeleteTicketResponse = void; // 204 빈 응답이라 void 처리
+
+export type UpdateTicketRequest = TicketId & DefaultTicketRequest;
+
+export type UpdateTicketResponse = DefaultTicketResponse;
+
+export type GetTicketBookRequest = ArchiveId &
+  DefaultPaginationRequest & {
+    sort?: PaginationSort;
+    direction?: PaginationDirection;
+  };
+
+export type GetTicketBookResponse = {
+  title: string;
+  content: TicketBookContent[];
+  page: DefaultPaginationResponse;
+};
+
+export type UpdateTicketBookRequest = ArchiveId & {
+  title: string;
+};
+
+export type UpdateTicketBookResponse = {
+  ticketBookId: number;
+  updatedTitle: string;
 };
