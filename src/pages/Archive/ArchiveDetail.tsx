8d2d2107b70237ff6e64fb02043803f7d48a87ae
jungmin <kjmhs501@gmail.com>
2026-01-31 19:38:38 +0900

    feat: 아카이브에서 일기, 갤러리, 티켓북, 리포스트 타이틀 수정 안되게 수정

--- src/pages/Archive/ArchiveDetail.tsx
+++ src/pages/Archive/ArchiveDetail.tsx
@@ -208,7 +208,7 @@ const ArchiveDetail = () => {
           />
           <div className="flex flex-col items-start justify-between gap-[60px] my-[60px]">
             {/* 덕질 일기 */}
-            <DiaryList archiveId={archiveId} limit={3} isOwner={archive?.isOwner} />
+            <DiaryList archiveId={archiveId} limit={3} isOwner={false} />
             {/* 덕질 갤러리 */}
             <ArchiveTitle
               title="덕질 갤러리"
@@ -217,7 +217,7 @@ const ArchiveDetail = () => {
                 navigate(`/archive/${archiveId}/gallery`);
               }}
               isMore={(archivedData?.Gallery?.length ?? 0) > 0}
-              isEditable={archive?.isOwner}
+              isEditable={false}
             />
             {(archivedData?.Gallery?.length ?? 0 > 0) ? (
               <GalleryList gallery={archivedData?.Gallery} />
@@ -236,7 +236,7 @@ const ArchiveDetail = () => {
             <TicketList
               archiveId={archiveId}
               limit={3}
-              isOwner={archive?.isOwner}
+              isOwner={false}
             />
             {/* 덕질 리포스트 */}
             <ArchiveTitle
@@ -246,7 +246,7 @@ const ArchiveDetail = () => {
                 navigate(`/archive/${archiveId}/repost`);
               }}
               isMore={(archivedData?.Repost?.length ?? 0) > 0}
-              isEditable={archive?.isOwner}
+              isEditable={false}
             />
             {(archivedData?.Repost?.length ?? 0 > 0) ? (
               <RepostList repost={archivedData?.Repost} />
