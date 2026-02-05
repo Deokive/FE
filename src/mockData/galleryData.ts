5a1b4e07cd292ba2628111b129ab6efafff82ca7
jungmin <kjmhs501@gmail.com>
2026-02-05 18:29:27 +0900

    feat: 아카이브 샘플데이터 갤러리 사진 변경

--- src/mockData/galleryData.ts
+++ src/mockData/galleryData.ts
@@ -2,51 +2,52 @@ const galleryDataMock = [
   {
     id: 90000000,
     archiveId: 90000000,
-    image: "https://picsum.photos/201/300",
+    image: "https://picsum.photos/210/300",
   },
   {
     id: 90000001,
     archiveId: 90000000,
-    image: "https://picsum.photos/202/300",
+    image: "https://picsum.photos/211/300",
   },
   {
     id: 90000002,
     archiveId: 90000000,
+    image: "https://picsum.photos/212/300",
   },
   {
     id: 90000003,
     archiveId: 90000000,
-    image: "https://picsum.photos/204/300",
+    image: "https://picsum.photos/213/300",
   },
   {
     id: 90000004,
     archiveId: 90000001,
-    image: "https://picsum.photos/205/300",
+    image: "https://picsum.photos/214/300",
   },
   {
     id: 90000005,
     archiveId: 90000001,
-    image: "https://picsum.photos/206/300",
+    image: "https://picsum.photos/215/300",
   },
   {
     id: 90000006,
     archiveId: 90000001,
-    image: "https://picsum.photos/207/300",
+    image: "https://picsum.photos/216/300",
   },
   {
     id: 90000007,
     archiveId: 90000001,
-    image: "https://picsum.photos/208/300",
+    image: "https://picsum.photos/217/300",
   },
   {
     id: 90000008,
     archiveId: 90000001,
-    image: "https://picsum.photos/209/300",
+    image: "https://picsum.photos/218/300",
   },
   {
     id: 90000009,
     archiveId: 90000001,
-    image: "https://picsum.photos/210/300",
+    image: "https://picsum.photos/219/300",
   },
 ];
 
