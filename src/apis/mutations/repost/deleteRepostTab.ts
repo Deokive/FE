7d521155ff607530d625cf98795a9e0b7d5df308
rhdmschdl <lovelyskygir@naver.com>
2026-02-06 00:48:43 +0900

    comment: 주석 삭제

--- src/apis/mutations/repost/deleteRepostTab.ts
+++ src/apis/mutations/repost/deleteRepostTab.ts
@@ -5,7 +5,6 @@ export async function deleteRepostTabApi(tabId: string | number) {
     throw new Error("tabId가 필요합니다.");
   }
   const url = `/api/v1/repost/tabs/${String(tabId)}`;
-  // DELETE /api/v1/repost/tabs/{tabId} - 성공 시 204 반환
   const res = await axiosInstance.delete(url);
   return res.data;
 }
